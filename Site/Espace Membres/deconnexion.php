606b9b3010b806d3998dbbbfeea4dfbcc53e74c1
Gwada <gwadagwada22@gmail.com>
2022-07-28 19:17:15 -0400

    Folders Organisations

---
+++ Site/Espace Membres/deconnexion.php
@@ -0,0 +1,11 @@
+<?php 
+    session_start();
+    session_destroy();
+    header('Location:index.php');
+<<<<<<< HEAD
+    die();
+=======
+    die();
+
+?>
+>>>>>>> b8b8d44 (27-07-2021; 09:11; Change database configuration and php session)
---
+++ Site/Espace Membres/js/WidthController.js
@@ -0,0 +1,46 @@
+const element = document.getElementById("prix")
+
+element.innerHTML = "Faite à: " + document.getElementById("js-back-1").clientWidth + "%";
+
+
+
+const element1 = document.getElementById("prix 1")
+
+element1.innerHTML ="Faite à: " +  document.getElementById("js-back 1").clientWidth + "%";
+
+
+
+const element2 =   document.getElementById("prix 2")
+
+element2.innerHTML ="Faite à: " +  document.getElementById("js-back 2").clientWidth + "%";
+
+
+
+const element3 =   document.getElementById("prix 3")
+
+element3.innerHTML ="Faite à: " +  document.getElementById("js-back 3").clientWidth + "%";
+
+
+
+const element4 =  document.getElementById("prix 4")
+
+element4.innerHTML ="Faite à: " +  document.getElementById("js-back 4").clientWidth + "%";
+
+
+
+const element5 =  document.getElementById("prix 5")
+
+element5.innerHTML ="Faite à: " +  document.getElementById("js-back 5").clientWidth + "%";
+
+
+
+const element6 =   document.getElementById("prix 6")
+
+element6.innerHTML ="Faite à: " +  document.getElementById("js-back 6").clientWidth + "%";
+
+
+
+
+const element7 =  document.getElementById("prix 7")
+
+element7.innerHTML ="Faite à: " +  document.getElementById("js-back 7").clientWidth + "%";
